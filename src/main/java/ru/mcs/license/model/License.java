9d824ef655f156ebe1099f4b112ff54e62ef0be0
Makhmud Shafeev <shafeevmkf@gmail.com>
2022-12-18 20:39:59 +0300

    Add link

--- src/main/java/ru/mcs/license/model/License.java
+++ src/main/java/ru/mcs/license/model/License.java
@@ -3,11 +3,13 @@ package ru.mcs.license.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.reactive.WebFluxLinkBuilder;
 
 @Getter
 @Setter
 @ToString
-public class License {
+public class License extends RepresentationModel<License> {
     private int id;
     private String licenseId;
     private String description;
